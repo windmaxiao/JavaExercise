874348c11bd1540876e708a333197eab7cd8be0f
windmaxiao <windxiao@vip.qq.com>
2022-12-06 23:28:02 +0800

    stream test code

--- src/main/java/com/example/demo/HelloWorld/TestJava8/StreamTest/StreamTest.java
+++ src/main/java/com/example/demo/HelloWorld/TestJava8/StreamTest/StreamTest.java
@@ -1,8 +1,8 @@
 package com.example.demo.HelloWorld.TestJava8.StreamTest;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
@@ -69,21 +69,93 @@ public class StreamTest {
     }
 
     public static void testStreamMap() {
-        List<String> list = Arrays.asList("aaa", "bbb", "ccc", "ddd", "eee");
+        List<String> list = Arrays.asList("aa", "bb", "cc", "dd", "ee");
+        System.out.println("map test");
         list.stream().map(String::toUpperCase).forEach(System.out::println);
+
+        System.out.println("map test2");
+        Stream<Stream<Character>> stream = list.stream().map(StreamTest::getCharacter);
+        stream.forEach(s -> s.forEach(System.out::println));
+
+        System.out.println("flatMap test");
+        Stream<Character> stream1 = list.stream().flatMap(StreamTest::getCharacter);
+        stream1.forEach(System.out::println);
     }
 
-    public static Stream<Character> getCharacter() {
+    public static Stream<Character> getCharacter(String str) {
+        List<Character> list = new ArrayList<>();
 
-        return null;
+        for (Character ch: str.toCharArray()) {
+            list.add(ch);
+        }
+
+        return list.stream();
     }
+
+    public static void testStreamSort() {
+        List<String> list = Arrays.asList("ee", "dddd", "cccc", "bbbbb", "a");
+
+        System.out.println("排序前");
+        list.forEach(System.out::println);
+
+        System.out.println("排序后-sorted()");
+        list.stream().sorted().forEach(System.out::println);
+
+        System.out.println("排序后-sorted(Comparator<? super T> comparator)");
+        list.stream().sorted((s1, s2) -> s2.length() - s1.length()).forEach(System.out::println);
+    }
+
+    public static void testStreamReduce() {
+        List<String> list = Arrays.asList("a", "b", "c", "d", "e");
+
+        String str = list.stream().reduce("reduce=", (x, y) -> x + y);
+
+        System.out.println(str);
+
+        List<User> users = Arrays.asList(new User("A", 1, true), new User("B", 2, true),
+                new User("C", 3, false), new User("D", 4, false));
+
+        Optional<Integer> sum = users.stream().map(User::getAge).reduce(Integer::sum);
+
+        System.out.println(sum.get());
+    }
+
+    public static void testStreamCollect() {
+        List<User> users = Arrays.asList(new User("A", 1, true), new User("B", 2, true),
+                new User("C", 3, false), new User("D", 4, false));
+
+        users.stream().map(User::getName).collect(Collectors.toList()).forEach(System.out::println);
+
+        Map<String, User> map = users.stream().collect(Collectors.toMap(User::getName, Function.identity()));
+        map.forEach((key, value) -> {
+            System.out.println(key + "==" + value.toString());
+        });
+
+        Map<Boolean, List<User>> map1 = users.stream().collect(Collectors.groupingBy(User::getOk));
+        map1.forEach((key, value) -> {
+            System.out.println(key + "==" + value);
+        });
+
+        String str = users.stream().map(User::getName).collect(Collectors.joining());
+        String str1 = users.stream().map(User::getName).collect(Collectors.joining(",", "+", "-"));
+        System.out.println(str);
+        System.out.println(str1);
+    }
+
 
 
     public static void main(String[] args) {
-        //testCreatStream();
 
-        //testStream();
+//        testCreatStream();
+//
+//        testStream();
+//
+//        testStreamMap();
+//
+//        testStreamSort();
+//
+//        testStreamReduce();
 
-        testStreamMap();
+        testStreamCollect();
     }
 }
---
+++ src/main/java/com/example/demo/HelloWorld/TestJava8/StreamTest/User.java
@@ -0,0 +1,18 @@
+package com.example.demo.HelloWorld.TestJava8.StreamTest;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/**
+ * @author maxiao
+ */
+@Data
+@AllArgsConstructor
+public class User {
+
+    private String name;
+
+    private int age;
+
+    private Boolean ok;
+}
