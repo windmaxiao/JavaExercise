7b81b9862cc374080efc780874287189ee80b062
马萧 <xma@makwing.com>
2023-05-09 16:27:30 +0800

    修改注释

--- src/main/java/com/example/demo/scheduler/config/ThreadPoolConfig.java
+++ src/main/java/com/example/demo/scheduler/config/ThreadPoolConfig.java
@@ -19,24 +19,26 @@ public class ThreadPoolConfig {
 
     @Bean(name = "asyncServiceExecutor")
     public Executor asyncServiceExecutor() {
-        //阿里巴巴编程规范：线程池不允许使用 Executors 去创建，而是通过 ThreadPoolExecutor 的方式，这样的处理方式让写的同学更加明确线程池的运行规则，规避资源耗尽的风险。
+        // 阿里巴巴编程规范：线程池不允许使用 Executors 去创建，
+        // 而是通过 ThreadPoolExecutor 的方式
+        // 这样的处理方式让写的同学更加明确线程池的运行规则，规避资源耗尽的风险。
 
-        //SpringBoot项目，可使用Spring提供的对 ThreadPoolExecutor 封装的线程池 ThreadPoolTaskExecutor：
+        // SpringBoot项目，可使用Spring提供的对 ThreadPoolExecutor 封装的线程池 ThreadPoolTaskExecutor：
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-//        ThreadPoolTaskExecutor executor = new MyThreadPoolTaskExecutor();//自定义ThreadPoolTaskExecutor，会打印线程池情况
-        //配置核心线程数
+        // 配置核心线程数
         executor.setCorePoolSize(5);
-        //配置最大线程数
+        // 配置最大线程数
         executor.setMaxPoolSize(10);
-        //配置队列大小
+        // 配置队列大小
         executor.setQueueCapacity(100);
         //配置线程池中的线程的名称前缀(方便排查问题)
         executor.setThreadNamePrefix("async-service-scheduler-");
         // rejection-policy：当pool已经达到max size的时候，如何处理新任务
-        //     1、CallerRunsPolicy：不在新线程中执行任务，而是由调用者所在的线程来执行。
+        //     1、CallerRunsPolicy:不在新线程中执行任务，而是由调用者所在的线程来执行。
         //        "该策略既不会抛弃任务，也不会抛出异常，而是将任务回推到调用者。"顾名思义，在饱和的情况下，调用者会执行该任务（而不是由多线程执行）
-        //     2、AbortPolicy：拒绝策略，直接拒绝抛出异常
-        //     3、。。。
+        //     2、AbortPolicy:拒绝策略，直接拒绝抛出异常
+        //     3、DiscardPolicy:丢弃任务，但是不抛出异常。可以配合这种模式进行自定义的处理方式
+        //     4、DiscardOldestPolicy:丢弃队列最早的未处理任务，然后重新尝试执行任务
         executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
         //执行初始化
         executor.initialize();
--- src/main/java/com/example/demo/scheduler/schedulers/SchedulerBlock.java
+++ src/main/java/com/example/demo/scheduler/schedulers/SchedulerBlock.java
@@ -16,28 +16,31 @@ import java.util.Date;
 @Component
 public class SchedulerBlock {
 
-
     /**
      * 定时任务1，每15秒执行一次，会执行10秒（造成10秒阻塞）
      * @throws InterruptedException InterruptedException
      */
-//    @Async(value = "asyncServiceExecutor")
-//    @Scheduled(cron = "0/15 * * * * ?")
-//    public void taskCron() throws InterruptedException {
-//        SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
-//        log.info("taskCron - 执行定时任务【从数据库中移除已删除超30天的数据】，时间： " + dateFormat.format(new Date()));
-//        //模拟延时
-//        Thread.sleep(10*1000);
-//    }
+    @Async(value = "asyncServiceExecutor")
+    @Scheduled(cron = "0/15 * * * * ?")
+    public void taskCron1() throws InterruptedException {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+        log.info("taskCron1 - 执行定时任务【0/15 * * * * ?】，时间： " + dateFormat.format(new Date()));
+        log.info("taskCron1 - 线程名称 " + Thread.currentThread().getName());
+        //模拟延时
+        Thread.sleep(10*1000);
+    }
+
+
 
     /**
      * 定时任务2，每3秒执行一次
      */
-//    @Async(value = "asyncServiceExecutor")
-//    @Scheduled(cron = "0/3 * * * * ?")
-//    public void taskFixedRate(){
-//        SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
-//        log.info("taskFixedRate - 执行定时任务【注销已超1天未支付的订单】，时间： " + dateFormat.format(new Date()));
-//    }
+    @Async(value = "asyncServiceExecutor")
+    @Scheduled(cron = "0/3 * * * * ?")
+    public void taskCron2(){
+        SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+        log.info("taskCron2 - 执行定时任务【0/3 * * * * ? 】，时间： " + dateFormat.format(new Date()));
+        log.info("taskCron2 - 线程名称 " + Thread.currentThread().getName());
+    }
 
 }
